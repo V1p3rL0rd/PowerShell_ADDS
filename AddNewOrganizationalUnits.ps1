c43831bbfb108b73a7bf5ac6ef26032ed3fcc518
Vianor Gvazbaya <123589830+V1p3rL0rd@users.noreply.github.com>
2025-03-03 23:36:36 +0300

    Add files via upload

---
+++ AddNewOrganizationalUnits.ps1
@@ -0,0 +1,20 @@
+﻿# Installing the RSAT AD DS module
+Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 
+
+# Importing the Active Directory module
+Import-Module ActiveDirectory
+
+# Specify the domain
+$domain = "garuda.net"
+
+# List of OUs to be created
+$OUs = @("Accounts", "HR", "IT", "Booking", "Transfer", "Operators")
+
+# Loop through each item in the list and create an OU
+foreach ($ou in $OUs) {
+    $ouPath = "OU=$ou,DC=garuda,DC=net"
+    New-ADOrganizationalUnit -Name $ou -Path "DC=garuda,DC=net" -ProtectedFromAccidentalDeletion $false
+    Write-Host "OU $ou created in the domain $domain"
+}
+
+Write-Host "All OUs have been created successfully!"
